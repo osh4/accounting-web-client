f97c276a01ded0b45a01dffdd9e7e56c5bf10c20
Konstantin Umanets <konstantin@osh4.com>
2022-12-21 00:58:40 +0400

    First test

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {SettingsListComponent} from './settings-list/settings-list.component';
+import {SettingCreateComponent} from './setting-create/setting-create.component';
+import {HomeComponent} from './home/home.component';
+
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "home"},
+  {path: "home", component: HomeComponent},
+  {path: "setting-create", component: SettingCreateComponent},
+  {path: "settings-list", component: SettingsListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
-import { AppComponent } from './app.component';
+import {SettingCreateComponent} from './setting-create/setting-create.component';
+import {SettingsListComponent} from './settings-list/settings-list.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {HomeComponent} from './home/home.component';
+// import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingCreateComponent,
+    SettingsListComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // RouterOutlet,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
