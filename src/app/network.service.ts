a73dab58e97e9a673fc98cdc3961acb0aa7b3acd
Konstantin Umanets <konstantin@osh4.com>
2022-12-28 00:44:17 +0400

    Add links to network service

--- src/app/network.service.ts
+++ src/app/network.service.ts
@@ -6,19 +6,38 @@ import {environment} from "../environments/environment";
 })
 export class NetworkService {
   private baseUrl = environment.baseUrl;
-  private settingsUrl: string = environment.settings;
-  private settingTypesUrl = environment.settingTypes;
 
   constructor() {
 
   }
 
   public getSettingsUrl(): string {
-    return this.baseUrl + this.settingsUrl;
+    return this.baseUrl + environment.settings;
   }
 
   public getSettingTypesUrl(): string {
-    return this.baseUrl + this.settingTypesUrl;
+    return this.baseUrl + environment.settingTypes;
   }
+
+  public getAccountsUrl(): string {
+    return this.baseUrl + environment.accounts;
+  }
+
+  public getCurrencyUrl(): string {
+    return this.baseUrl + environment.currency;
+  }
+
+  public getTransactionsUrl(): string {
+    return this.baseUrl + environment.transactions;
+  }
+
+  public getTransactionTypesUrl(): string {
+    return this.baseUrl + environment.transactionTypes;
+  }
+
+
+  // public getAccountTypesUrl(): string {
+  //   return this.baseUrl + environment.accountTypes;
+  // }
 
 }
