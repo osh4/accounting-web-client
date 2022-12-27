2b5bdf904a63e042a09babf997788f40873d8599
Konstantin Umanets <konstantin@osh4.com>
2022-12-28 00:42:26 +0400

    Fill the navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AccSettingsListComponent} from './settings/acc-settings-list/acc-settings-list.component';
-import {HomeComponent} from './home/home.component';
+import {DashboardComponent} from './acc-dashboard/dashboard.component';
 import {AccIncomeComponent} from "./acc-income/acc-income.component";
+import {AccAccountsListComponent} from "./acc-accounts-list/acc-accounts-list.component";
+import {AccTransactionsListComponent} from "./acc-transactions-list/acc-transactions-list.component";
+import {AccTransactionTypesListComponent} from "./acc-transaction-types-list/acc-transaction-types-list.component";
 
 const routes: Routes = [
-  {path: "", pathMatch: "full", redirectTo: "home"},
-  {path: "home", component: HomeComponent},
+  {path: "", pathMatch: "full", redirectTo: "dashboard"},
+  {path: "dashboard", component: DashboardComponent},
   {path: "settings-list", component: AccSettingsListComponent},
-  {path: "income", component: AccIncomeComponent}
+  {path: "income", component: AccIncomeComponent},
+  {path: "accounts", component: AccAccountsListComponent},
+  {path: "transactions", component: AccTransactionsListComponent},
+  {path: "transaction-types", component: AccTransactionTypesListComponent},
+  {path: "account-types", component: AccAccountsListComponent}
 ];
 
 @NgModule({
