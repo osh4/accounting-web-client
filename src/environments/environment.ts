278fed50b5a9ab76020ff9182c0cdb0fd63ec104
Konstantin Umanets <konstantin@osh4.com>
2022-12-28 19:21:34 +0400

    Fix build for dev and prod envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,10 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:8443',
-  settings: '/setting',
-  settingTypes: '/setting/type'
+  baseUrl: '',
+  settings: '',
+  settingTypes: '',
+  accounts: '',
+  currency: '',
+  transactions: '',
+  transactionTypes: ''
 };
